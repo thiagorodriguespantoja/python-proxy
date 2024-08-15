ecdbd6553dce31ba23d5fb3e003a3b68d41f8055
Thiago Pantoja <thiago.pantoja@enkel.com.br>
2024-08-15 15:09:37 -0300

    🔧 [Fix] - Ajuste na estrutura de importação para testes
    ✅ Corrigido o caminho de importação da função `login_autenticacao` no arquivo `test_auth.py`:
    - Adicionado `sys.path.insert()` para garantir que o diretório `app` esteja no caminho de importação do Python.
    - Agora a função `login_autenticacao` pode ser importada corretamente para ser testada no pytest.
    
    🧪 Testes:
    - Teste `test_auth()` verificado e pronto para execução com pytest.
    
    #tags: #python #pytest #importfix #tests #initialTest

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,2 +1,11 @@
+import sys
+import os
+
+# Adiciona o diretório "app" ao caminho de pesquisa do Python
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'app')))
+
+# Agora você pode importar a função login_autenticacao
+from auth import login_autenticacao
+
 def test_auth():
     assert login_autenticacao("user", "pass", 1) == True
